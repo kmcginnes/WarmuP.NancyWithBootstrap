194e4cce3af19115c92ac9902ae49ae36c51fcf2
Kris McGinnes <kmcginnes@me.com>
2013-06-08 10:28:00 -0500

    Initial commit

---
+++ __NAME__.Web/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+using Nancy.Conventions;
+
+namespace __NAME__.Web
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+            nancyConventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("Scripts"));
+        }
+    }
+}
---
+++ __NAME__.Web/HomeModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace __NAME__.Web
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => View["Index"];
+        }
+    }
+}
